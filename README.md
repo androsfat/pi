4be1dd68210b0c3bf03b1c0422da05a6bce46d26
androsfat <androskalu@gmail.com>
2017-04-15 14:05:42 +0300

    Add calculation of pi

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # pi
+
+Calculation of pi using the fraction 245850922 / 78256779
---
+++ pi.py
@@ -0,0 +1,6 @@
+# Even though if you use the math.pi from python
+# or the below fraction (that is it the most accurate one)
+# you will only get until the 15th decimal point
+pi = 245850922 / 78256779
+
+print(pi)
